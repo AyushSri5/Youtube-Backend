ab3d50e3467ab148a948c9f60c657cf0ef03c594
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-06-22 22:11:17 +0530

    Add new routes for user.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -414,7 +414,7 @@ const getWatchHistory = asyncHandler(async (req,res) => {
     const user = await User.aggregate([
         {
             $match:{
-                _id:new mongoose.Types.ObjectId(req.)
+                _id:new mongoose.Types.ObjectId(req.user._id)
             }
         },
         {
